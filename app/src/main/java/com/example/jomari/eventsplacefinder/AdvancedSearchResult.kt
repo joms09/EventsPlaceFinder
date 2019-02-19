1fee52fcd1045a66f8b5898535fc002b45dccbd9
joms09 <jomari.eneldas7@gmail.com>
2019-02-19 12:16:52 +0800

    need to fix err

--- app/src/main/java/com/example/jomari/eventsplacefinder/AdvancedSearchResult.kt
+++ app/src/main/java/com/example/jomari/eventsplacefinder/AdvancedSearchResult.kt
@@ -6,11 +6,13 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import com.example.jomari.eventsplacefinder.NewMessageActivity.Companion.USER_KEY
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
+import com.google.firebase.firestore.FirebaseFirestore
 import com.xwray.groupie.GroupAdapter
 import com.xwray.groupie.Item
 import com.xwray.groupie.ViewHolder
@@ -18,6 +20,7 @@ import kotlinx.android.synthetic.main.advance_search_result.*
 import kotlinx.android.synthetic.main.filter_results.view.*
 
 class AdvancedSearchResult : AppCompatActivity() {
+
 
     val resultRef = FirebaseDatabase.getInstance().getReference("event")
 
@@ -28,6 +31,7 @@ class AdvancedSearchResult : AppCompatActivity() {
         setContentView(R.layout.advance_search_result)
 
         supportActionBar?.title = "Search Results"
+        val db = FirebaseFirestore.getInstance()
 
 
         val placeid = intent.getStringExtra("id")
@@ -51,54 +55,98 @@ class AdvancedSearchResult : AppCompatActivity() {
         eventLabel.text = type
         minibudgetLabel.text = minibudget
 
-        val filterResult = resultRef.orderByChild("type").equalTo(type)
-        filterResult.addValueEventListener(object : ValueEventListener {
-            override fun onCancelled(databaseError: DatabaseError) {
+        val ref = db.collection("event")
+        ref.whereEqualTo("type", type)
+        ref.whereLessThanOrEqualTo("max_people", capacity1)
+        ref.whereGreaterThanOrEqualTo("min_price", minibudget)
 
-            }
+        ref.get().addOnSuccessListener { documents ->
+            val adapter = GroupAdapter<ViewHolder>()
+            for (document in documents) {
+                Log.d(
+                    "HERE LOOK AT HERE",
+                    document[type].toString() +
+                            document[capacity1] as Throwable? +
+                            document[minibudget]
+                )
+                documents.forEach { documentSnapshot ->
+                    run {
+                        documentSnapshot.data.let { model ->
+                            val item = SearchItem(model)
+                            item.result.Id = documentSnapshot.id
+                            adapter.add(item)
+                        }
 
-            override fun onDataChange(dataSnapshot: DataSnapshot) {
-
-                val adapter = GroupAdapter<ViewHolder>()
-
-                dataSnapshot.children.forEach {data ->
-                    @Suppress("NestedLambdaShadowedImplicitParameter")
-                    data.getValue(Model::class.java)?.let {model ->
-                        val item = SearchItem(model)
-                        item.result.Id = data.key
-                        adapter.add(item)
                     }
-                }
 
-                adapter.setOnItemClickListener { item, view ->
-                    val searchItem = item as SearchItem
-                    val intent = Intent(view.context, SoloDetailsBySearch::class.java)
-                    intent.putExtra(USER_KEY, searchItem.result)
-                    intent.putExtra("id", placeid)
-                    intent.putExtra("name", name)
-                    intent.putExtra("status", status)
-                    intent.putExtra("type", type)
-                    intent.putExtra("address", address)
-                    intent.putExtra("count", count)
-                    intent.putExtra("image", image)
-                    startActivity(intent)
-                    finish()
+                    adapter.setOnItemClickListener { item, view ->
+                        val searchItem = item as SearchItem
+                        val intent = Intent(view.context, SoloDetailsBySearch::class.java)
+                        intent.putExtra(USER_KEY, searchItem.result)
+                        intent.putExtra("id", placeid)
+                        intent.putExtra("name", name)
+                        intent.putExtra("status", status)
+                        intent.putExtra("type", type)
+                        intent.putExtra("address", address)
+                        intent.putExtra("count", count)
+                        intent.putExtra("image", image)
+                        startActivity(intent)
+                        finish()
+                    }
+                    recyclerview_view_result.adapter = adapter
                 }
-                recyclerview_view_result.adapter = adapter
             }
-        })
+
+//            val filterResult = resultRef.orderByChild("type").equalTo(type)
+//            filterResult.addValueEventListener(object : ValueEventListener {
+//                override fun onCancelled(databaseError: DatabaseError) {
+//
+//                }
+//
+//                override fun onDataChange(dataSnapshot: DataSnapshot) {
+//
+//                    val adapter = GroupAdapter<ViewHolder>()
+
+//                dataSnapshot.children.forEach {data ->
+//                    @Suppress("NestedLambdaShadowedImplicitParameter")
+//                    data.getValue(Model::class.java)?.let {model ->
+//                        val item = SearchItem(model)
+//                        item.result.Id = data.key
+//                        adapter.add(item)
+//                    }
+//                }
+
+//                    adapter.setOnItemClickListener { item, view ->
+//                        val searchItem = item as SearchItem
+//                        val intent = Intent(view.context, SoloDetailsBySearch::class.java)
+//                        intent.putExtra(USER_KEY, searchItem.result)
+//                        intent.putExtra("id", placeid)
+//                        intent.putExtra("name", name)
+//                        intent.putExtra("status", status)
+//                        intent.putExtra("type", type)
+//                        intent.putExtra("address", address)
+//                        intent.putExtra("count", count)
+//                        intent.putExtra("image", image)
+//                        startActivity(intent)
+//                        finish()
+//                    }
+//                    recyclerview_view_result.adapter = adapter
+        }
+        //           })
+    }
+//    }
+
+    class SearchItem(val result: Model) : Item<ViewHolder>() {
+
+        override fun bind(viewHolder: ViewHolder, position: Int) {
+            viewHolder.itemView.textview_event_type.text = result.Type
+            viewHolder.itemView.textview_event_name.text = result.Name
+        }
+
+        override fun getLayout(): Int {
+            return R.layout.filter_results
+        }
+
     }
 }
-
-class SearchItem(val result: Model) : Item<ViewHolder>() {
-
-    override fun bind(viewHolder: ViewHolder, position: Int) {
-        viewHolder.itemView.textview_event_type.text = result.Type
-        viewHolder.itemView.textview_event_name.text = result.Name
-    }
-
-    override fun getLayout(): Int {
-        return R.layout.filter_results
-    }
-
-}
+//}
